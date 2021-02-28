0d0ab87919f6dcd1d19baab4ffb083393a2a8140
Andwerpz <76826055+Andwerpz@users.noreply.github.com>
2021-02-27 23:58:02 -0600

    wormsort

---
+++ USACO Practice/src/wormsort/wormsort.java
@@ -0,0 +1,194 @@
+/*
+ID: andwerp2
+LANG: JAVA
+TASK: wormsort
+*/
+
+//package wormsort;
+
+import java.io.*;
+import java.util.*;
+
+class wormsort {
+	
+	static boolean isCompleteGraph(ArrayList<Integer> misplacedCows, int[][] wormholes, int pointer) {
+		
+		ArrayList<Integer> inGraph = new ArrayList<Integer>();
+	    HashSet<Integer> visited = new HashSet<Integer>();
+	    
+	    boolean noConnection = false;
+	    
+	    int[] firstConnection = wormholes[pointer];
+	    
+	    inGraph.add(firstConnection[0]);
+	    inGraph.add(firstConnection[1]);
+	    
+	    visited.add(firstConnection[0]);
+	    visited.add(firstConnection[1]);
+	    
+	    while(!noConnection) {
+	    	
+	    	noConnection = true;
+	    	
+	    	
+	    	
+	    	for(int i = 0; i < inGraph.size(); i++) {
+	    		
+	    		////System.out.println(i);
+	    		
+	    		for(int j = pointer; j < wormholes.length; j++) {
+	    			
+	    			if(wormholes[j][0] == inGraph.get(i) && !visited.contains(wormholes[j][1])) {
+	    				
+	    				inGraph.add(wormholes[j][1]);
+	    				visited.add(wormholes[j][1]);
+	    				noConnection = false;
+	    				
+	    			}
+	    			
+	    			else if(wormholes[j][1] == inGraph.get(i) && !visited.contains(wormholes[j][0])) {
+	    				
+	    				inGraph.add(wormholes[j][0]);
+	    				visited.add(wormholes[j][0]);
+	    				noConnection = false;
+	    				
+	    			}
+	    			
+	    		}
+	    		
+	    	}	
+	    	
+	    }
+	    
+	    boolean isCompleteGraph = true;
+	    
+	    for(int i : inGraph) {
+	    	//System.out.println(i + " in graph");
+	    }
+	    
+	    for(int i = 0; i < misplacedCows.size(); i++) {
+	    	if(!visited.contains(misplacedCows.get(i))) {
+	    		isCompleteGraph = false;
+	    		break;
+	    	}
+	    }
+	    
+	    ////System.out.println(isCompleteGraph);
+	    
+	    return isCompleteGraph;
+		
+	}
+	
+	
+	
+	
+	public static void main (String [] args) throws IOException {
+    
+    BufferedReader fin = new BufferedReader(new FileReader("wormsort.in"));
+    PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("wormsort.out")));
+    StringTokenizer st = new StringTokenizer(fin.readLine());
+    
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    
+    int[] cows = new int[n];
+    int[][] wormholes = new int[m][3];
+    
+    int numMisplacedCows = 0;
+    
+    ArrayList<Integer> misplacedCows = new ArrayList<Integer>();
+    
+    st = new StringTokenizer(fin.readLine());
+    
+    for(int i = 0; i < n; i++) {
+    	
+    	cows[i] = Integer.parseInt(st.nextToken()) - 1;
+    	
+    	if(cows[i] != i) {
+    		
+    		numMisplacedCows ++;
+    		misplacedCows.add(i);
+    		
+    	}
+    	
+    }
+    
+    if(misplacedCows.size() == 0) {
+    	
+    	fout.println(-1);
+    }
+    
+    else {
+    	
+	    for(int i : misplacedCows) {
+	    	
+	    	//System.out.println(i + " is misplaced");
+	    }
+	    
+	    for(int i = 0; i < m; i++) {
+	    	
+	    	st = new StringTokenizer(fin.readLine());
+	    	
+	    	wormholes[i] = new int[] {Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()),};
+	    	
+	    }
+	    
+	    Arrays.sort(wormholes, (a, b) -> a[2] - b[2]);
+	    
+	    for(int[] i : wormholes) {
+	    	
+	    	//System.out.println(i[0] + " " + i[1] + " " + i[2]);
+	    	
+	    }
+	    
+	    boolean isMaxSize = false;
+	    
+	    int high = m - 1;
+	    int low = 0;
+	    
+	    int pointer = (high + low) / 2;
+	    
+	    int maxSize = 0;
+	    
+	    while(!isMaxSize) {
+	    	
+		    
+		    
+		    if(isCompleteGraph(misplacedCows, wormholes, pointer)) {
+		    	
+		    	if(!isCompleteGraph(misplacedCows, wormholes, pointer + 1)) {
+		    		
+		    		maxSize = wormholes[pointer][2];
+		    		isMaxSize = true;
+		    		break;
+		    		
+		    	}
+		    	
+		    	else {
+		    		
+		    		low = pointer;
+		    		pointer = (high + low) / 2;
+		    		
+		    	}
+		    	
+		    }
+		    
+		    else {
+		    	
+		    	high = pointer;
+		    	pointer = (high + low) / 2;
+		    	
+		    }
+	    	
+	    }
+	    
+	    fout.println(maxSize);
+    }
+    
+    
+    
+    fin.close();
+    fout.close();
+    
+	}
+}

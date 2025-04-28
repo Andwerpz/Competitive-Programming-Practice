f8f917b8633330dae47d220f8c05adfbb35d6474
Andwerpz <andwerpooni@gmail.com>
2025-04-28 10:28:28 -0500

    Create copyorprefixsum.cpp

---
+++ - cpp -/copyorprefixsum.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl; 
+typedef vector<bool> vb;
+typedef vector<ld> vd;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<bool>> vvb;
+typedef vector<vector<ld>> vvd;
+// typedef __int128 lll;
+// typedef __float128 lld;
+
+//Codeforces - 1485F
+
+struct mint;
+typedef vector<mint> vm;
+typedef vector<vector<mint>> vvm;
+typedef pair<mint, mint> pmm;
+
+const ll mod = 1e9 + 7;
+struct mint {
+    ll val; //this should always be in range [0, mod)
+    mint(ll _val = 0) {val = _val; if(val < 0) val = mod + (val % mod); val %= mod;}
+    mint(const mint& other) {val = other.val;}
+    bool operator ==(const mint& other) const {return val == other.val;}
+    bool operator ==(ll other) const {return val == other;}
+    bool operator !=(const mint& other) const {return val != other.val;}
+    bool operator !=(ll other) const {return val != other;}
+    bool operator >(const mint& other) const {return val > other.val;}
+    bool operator >(ll other) const {return val > other;}
+    bool operator <(const mint& other) const {return val < other.val;}
+    bool operator <(ll other) const {return val < other;}
+    mint& operator =(const mint& other) {val = other.val; return *this;}
+    mint& operator =(ll other) {val = other; return *this;}
+    mint operator +(const mint& other) const {ll ret = val + other.val; while(ret >= mod) {ret -= mod;} return mint(ret);}
+    mint operator +(ll other) const {return *this + mint(other);}
+    mint& operator +=(const mint& other) {*this = *this + other; return *this;}
+    mint& operator +=(ll other) {*this = *this + other; return *this;}
+    mint operator -(const mint& other) const {ll ret = val - other.val; while(ret < 0) {ret += mod;} return mint(ret);}
+    mint operator -(ll other) const {return *this - mint(other);}
+    mint& operator -=(const mint& other) {*this = *this - other; return *this;}
+    mint& operator -=(ll other) {*this = *this - other; return *this;}
+    mint operator *(const mint& other) const {return mint((val * other.val) % mod);}
+    mint operator *(ll other) const {return *this * mint(other);}
+    mint& operator *=(const mint& other) {*this = *this * other; return *this;}
+    mint& operator *=(ll other) {*this = *this * other; return *this;}
+    mint operator /(const mint& other) const {return *this * other.pow(mod - 2);}
+    mint operator /(ll other) const {return *this / mint(other);}
+    mint& operator /=(const mint& other) {*this = *this / other; return *this;}
+    mint& operator /=(ll other) {*this = *this / other; return *this;}
+    mint operator %(const mint& other) const {return mint(val % other.val);}
+    mint operator %(ll other) const {return *this % mint(other);}
+    mint& operator %=(const mint& other) {*this = *this % other; return *this;}
+    mint& operator %=(ll other) {*this = *this % other; return *this;}
+
+    //don't forget about fermat's little theorem, 
+    //a^(m-1) % m = 1. This means that a^(p % m) % m != a^(p) % m, rather a^(p % (m-1)) % m = a^(p) % m. 
+    mint pow(ll b) const {
+        mint ans(1), p(val);
+        while(b != 0) {if(b % 2 == 1) {ans *= p;} p *= p; b /= 2;}
+        return ans;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const mint& m) {os << m.val; return os;}
+    friend std::istream& operator>>(std::istream& is, mint& m) {is >> m.val; return is;}
+    operator size_t() const {return val;}
+};
+bool operator ==(ll a, const mint& b) {return mint(a) == b;}
+bool operator !=(ll a, const mint& b) {return mint(a) != b;}
+bool operator >(ll a, const mint& b) {return mint(a) > b;}
+bool operator <(ll a, const mint& b) {return mint(a) < b;}
+mint operator +(ll a, const mint& b) {return mint(a) + b;}
+mint operator -(ll a, const mint& b) {return mint(a) - b;}
+mint operator *(ll a, const mint& b) {return mint(a) * b;}
+mint operator /(ll a, const mint& b) {return mint(a) / b;}
+mint operator %(ll a, const mint& b) {return mint(a) % b;}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vl a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+        vm dp(n);
+        map<ll, mint> m;
+        m[0] = 1;
+        mint sum = 1;
+        ll pfx = 0;
+        for(int i = 0; i < n - 1; i++){
+            dp[i] = sum - m[pfx];
+            m[pfx] += dp[i];
+            sum += dp[i];
+            pfx += a[i];
+        }
+        mint ans = 2 * sum - m[pfx];
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
